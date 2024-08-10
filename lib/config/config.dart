ee5399067e654d6c2abe7243771561a7cc907a64
Daniel Adel <danieladel217@gmail.com>
2024-08-10 11:11:14 +0200

    frist commit

---
+++ lib/config/config.dart
@@ -0,0 +1,41 @@
+
+// class Config {
+//   static String websiteURL = 'https://zacelectronic.com/';
+//   static String key = 'ck_b38f0f831b2f8507d710b9833655fd8501f10ac2';
+//   static String secret = 'cs_4e4e6174cb964eea30b3a859857edbd29e4b4d02';
+//   static String url = 'https://zacelectronic.com/wp-json/wc/v3/';
+//   static String orderConfirmUrl = 'https://zacelectronic.com/checkout/order-received/';
+//   static String customerURL = 'customers';
+//   static String tokenURL = 'https://zacelectronic.com/wp-json/jwt-auth/v1/token';
+//   static String categoryURL = 'products/categories';
+//   static String productsURL = 'products';
+//   static String singleProductsURL = 'products/';
+//   static String customerDetails = 'customers/';
+//   static String createOrderUrl = 'orders';
+//   static String anOrderUrl = 'orders/';
+//   static String coupons = 'coupons';
+//   static String media = 'https://zacelectronic.com/wp-json/wp/v2/media?';
+//   static String createReviewUrl = 'products/reviews';
+//
+//
+// }
+class Config {
+  // static String websiteURL = 'https://zacelectronic.com/';
+  static String websiteURL = 'https://zacelectronic.com/';
+  static String key = 'ck_d5f23c59516ad5e1494f6bc4948513004b42048e';
+  static String secret = 'cs_5e590c5079bf0ba1a044876932f552cadd33549b';
+  static String url = 'https://zacelectronic.com/';
+  static String customerURL = 'customers';
+  static String tokenURL = '#';
+  static String orderConfirmUrl = 'https://zacelectronic.com/checkout/order-received/';
+  static String categoryURL = 'products/categories';
+  static String productsURL = 'products';
+  static String singleProductsURL = 'products/';
+  static String customerDetails = 'customers/';
+  static String createOrderUrl = 'orders';
+  static String anOrderUrl = 'orders/';
+  static String coupons = 'coupons';
+  static String media = 'https://zacelectronic.com/wp-json/wp/v2/media?per_page=100';
+  static String createReviewUrl = 'products/reviews';
+}
+
